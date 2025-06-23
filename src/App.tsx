372944a4514034ba191c3b8f36db6ee6964683a1
asamiy-wni <asamiy@wni.com>
2025-06-24 07:20:15 +0900

    remove spinner

--- src/App.tsx
+++ src/App.tsx
@@ -58,12 +58,6 @@ const WeatherCard: React.FC<WeatherCardProps> = ({
   </div>
 );
 
-const LoadingSpinner: React.FC = () => (
-  <div className="flex items-center justify-center">
-    <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"></div>
-  </div>
-);
-
 const StatusIndicator: React.FC<{ 
   isLoading: boolean; 
   isError: boolean; 
