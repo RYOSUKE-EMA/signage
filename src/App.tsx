01175775545031f94b24be317568778e042fbbcd
asamiy-wni <asamiy@wni.com>
2025-08-23 10:49:41 +0900

    com in dummydata

--- src/App.tsx
+++ src/App.tsx
@@ -182,12 +182,12 @@ const getRandomNumber = (min: number, max: number, decimals: number = 1): number
   return Math.round((Math.random() * (max - min) + min) * factor) / factor;
 };
 
-// // 都市ごとのダミーデータを定義
-// const cityData: { [key: string]: string } = {
-//   tokyo: '東京都',
-//   osaka: '大阪府',
-//   nagoya: '愛知県'
-// };
+// 都市ごとのダミーデータを定義
+const cityData: { [key: string]: string } = {
+  tokyo: '東京都',
+  osaka: '大阪府',
+  nagoya: '愛知県'
+};
 
 const getDummyWeatherData = (city: string): WeatherData => {
   let temperatureRange, windRange;
