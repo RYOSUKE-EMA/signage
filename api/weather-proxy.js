77e39154e4c9a41cf990b564ec2b15c295612032
RYOSUKE-EMA <ryosuke.ema@suyama-group.co.jp>
2026-06-01 09:33:01 +0900

    Update weather-proxy.js

--- api/weather-proxy.js
+++ api/weather-proxy.js
@@ -1,12 +1,23 @@
-import { locationData } from '../../src/locationData.ts';
-
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   const pathSegments = req.url.split('/');
   const location = pathSegments[pathSegments.length - 1];
-  const deviceId = locationData[location].deviceId;
-  const externalApiUrl = `https://soratena.weathernews.jp/api/v1/data/latest?deviceId=${deviceId}`;
+
+  const locationData = {
+    hamako: { displayName: '浜松工業高等学校', deviceId: 'ST3OJE00-000392' },
+    merck:  { displayName: 'メルクエレクトロニクス', deviceId: 'ST3OJE00-000577' },
+    hamazoo: { displayName: '浜松市動物園', deviceId: 'ST3OJE00-000705' },
+    mikumi: { displayName: '三組倉庫', deviceId: 'ST3OJE00-000984' },
+  };
+
+  const locationInfo = locationData[location];
+
+  if (!locationInfo || locationInfo.deviceId === 'unknown') {
+    return res.status(404).json({ message: 'Location not found' });
+  }
+
+  const externalApiUrl = `https://soratena.weathernews.jp/api/v1/data/latest?deviceId=${locationInfo.deviceId}`;
   const apiKey = 'AXCI2Liuyu94PGpEl46cEa7Ck2SU0Xbv3mDc8SNi';
 
   try {
